1484cd73f4f360d5caba36f2afb9848e9f107038
Gábor <deutschgabor92@gmail.com>
2023-08-27 15:53:17 +0200

    docker compose, dockerfile and vite config update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    origin: "http://127.0.0.1:8000",
+  // server: {
+  //   host: true,
+  //   strictPort: true,
+  //   port: 5174,
+  // },
+  preview: {
     host: true,
-    strictPort: true,
     port: 5174,
   },
-  base: "/",
 });
